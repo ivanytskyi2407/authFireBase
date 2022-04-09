bc76176a52c7543f526246f1a408e746cabffbbb
ivanytskyi2407 <ivanytskyi2407@gmail.com>
2022-04-09 15:48:49 +0300

    Update fireStore.js

--- src/fireStore.js
+++ src/fireStore.js
@@ -1,16 +1,16 @@
-import { app, auth } from './auth';
-import { getFirestore, collection, getDocs, getDoc, addDoc } from 'firebase/firestore';
-import { onAuthStateChanged } from 'firebase/auth';
+// import { app, auth } from './auth';
+// import { getFirestore, collection, getDocs, getDoc, addDoc } from 'firebase/firestore';
+// import { onAuthStateChanged } from 'firebase/auth';
 
-const db = getFirestore(app);
-const films = collection(db, 'films');
-const snapshot = await getDocs(films);
+// const db = getFirestore(app);
+// const films = collection(db, 'films');
+// const snapshot = await getDocs(films);
 
-// detect auth state
-onAuthStateChanged(auth, user => {
-  if (user !== null) console.log('logged in');
-  else {
-    console.log('no User');
-  }
-});
-onAuthStateChanged();
+// // detect auth state
+// onAuthStateChanged(auth, user => {
+//   if (user !== null) console.log('logged in');
+//   else {
+//     console.log('no User');
+//   }
+// });
+// onAuthStateChanged();
